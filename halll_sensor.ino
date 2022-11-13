b9cc0db1ad0e79e1befcec663cd81df9a23967e8
SachaWheeler <sacha@sachawheeler.com>
2022-11-13 00:35:00 +0000

    dealy of one second

--- halll_sensor.ino
+++ halll_sensor.ino
@@ -1,7 +1,7 @@
 int hallSensorPin = 4;
 int hallSensorValue = 0;
 
-int period = 10 * 1000; // 10 seconds
+int period = 1 * 1000; // 1 second
 unsigned long time_now = 0;
 unsigned long trigger_time = 0;
 bool trigger = true;
@@ -22,12 +22,12 @@ void loop() {
 
   if (trigger == true && hallSensorValue == 0) {
     // send a serial message and only do this once per second
-    Serial.print("trigger");
+    Serial.println("trigger");
 
     trigger_time = millis();
     trigger = false;
   }
-  if (trigger == false && millis() > trigger_time + period) {
+  if (trigger == false && time_now > trigger_time + period) {
     // turn it on again
     trigger = true;
   }
